de66363c64510ca37b48dab1cdfdafebcf0aee56
kyo <ayman.ben-souira@student-cs.fr>
2023-02-16 10:33:11 +0000

    camera

--- ff.py
+++ ff.py
@@ -3,11 +3,38 @@ import numpy
 import torch
 import re
 
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
 
 model = torch.hub.load("ultralytics/yolov5", 'yolov5s')
 
 #cv2.startWindowThread()
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 i = 0
 while True:
     check, frame = video.read()
